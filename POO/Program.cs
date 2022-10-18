0c8a724b56001bcfe6e096fb9c9731763a09d72b
Egolpe <enriquegolpe@gmail.com>
2022-10-18 10:26:41 +0200

    second commit

--- POO/Program.cs
+++ POO/Program.cs
@@ -1,6 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using POO;
+// Crear objeto a partir de una clase
+
+Employee employee  = new Employee("684748494J", "empleado1", 30, 3000.0, false);
+Employee employee1 = new Employee("938493847T", "empleado2", 28, 2900.0, true);
+
+Console.WriteLine(employee1.Name);
+Console.WriteLine(employee.Name);
+
+Console.WriteLine(employee.Salary + employee1.Salary);
+var media = (employee.Salary + employee1.Salary) / 2;
+Console.WriteLine(media);
+
+Employee employee2 = new Employee("11111111G", "empleado3");
+Employee employee3 = new Employee("22222222G", "empleado4");
+Console.WriteLine(employee2.Name);
+Console.WriteLine(employee3.Name);
+
+Employee employee4 = new Employee("333333333T", "empleado5", 3100.0);
+Console.WriteLine(employee4.Salary);
+
+Employee employee5 = new Employee();
+employee5.Name = "empleado6";
