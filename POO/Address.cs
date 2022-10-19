635966fbdb071ae3970a0b96d3e9bd240caaeef9
Egolpe <enriquegolpe@gmail.com>
2022-10-19 11:30:44 +0200

    Herencia y abstarccion de clases

---
+++ POO/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace POO;
+
+public class Address
+{
+    public string street;
+    public string PostalCode;
+    public string City;
+    public string Country;
+
+
+    public Address()
+    {
+    }
+}
+
