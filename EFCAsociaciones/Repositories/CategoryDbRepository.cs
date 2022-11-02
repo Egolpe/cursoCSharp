3aebd1f3371da90dd3ba503dfb3f1b7fdf1ce720
Egolpe <enriquegolpe@gmail.com>
2022-11-02 13:52:40 +0100

    EFC Repositorios

---
+++ EFCAsociaciones/Repositories/CategoryDbRepository.cs
@@ -0,0 +1,56 @@
+﻿
+using EFCAsociaciones.Models;
+
+namespace EFCAsociaciones.Repositories;
+
+public class CategoryDbRepository : ICategoryRepository
+{
+    private AppDbContext Context;
+    //constructores
+    public CategoryDbRepository(AppDbContext context)
+    {
+        Context = context;
+    }
+
+    public CategoryDbRepository()
+    {
+    }
+
+    public Category Create(Category category)
+    {
+        if (category.Id > 0)
+        {
+            return Update(category);
+        }
+
+
+        Context.Categories.Add(category);// un libro puede tener author y categories
+        Context.SaveChanges();
+        return category;
+    }
+
+    public List<Category> FindAll()
+    {
+        return Context.Categories.ToList();
+    }
+
+    public Category FindById(int id)
+    {
+        return Context.Categories.Find(id);
+    }
+
+    public bool Remove(int id)
+    {
+        Category category = FindById(id);
+        if (category == null)
+        {
+            return false;
+        }
+        Context.Categories.Remove(category);// un libro puede tener author y categories
+        Context.SaveChanges();
+        return true;
+    }
+
+   
+}
+
---
+++ EFCAsociaciones/Repositories/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿
+namespace EFCAsociaciones.Repositories;
+
+public interface IAddressRepository
+{
+    //buscar por id
+    Address FindById(int id);
+
+    //Buscar todos
+    List<Address> FindAll();
+
+    //guardar
+    Address Create(Address address);
+    //actualizar  restringiendo campos
+    Address Update(Address address);
+    //borrar por id
+    bool Remove(int id);
+}
+
