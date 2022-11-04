5110a820548ebca7f64d05cb733d3e4135bc2fbf
Egolpe <enriquegolpe@gmail.com>
2022-11-04 12:04:54 +0100

    ASP.NET Book Controller

---
+++ ASPNET1/Controllers/BookController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ASPNET1.Controllers
+{
+    public class BookController
+    {
+        public BookController()
+        {
+        }
+    }
+}
+
---
+++ ASPNET1/Controllers/HelloController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET1.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class HelloController
+{
+
+    public HelloController()
+    {
+    }
+
+    //GET https://localhost:7279/api/hello/saludo
+    [HttpGet("saludo")]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+    //GET https://localhost:7279/api/hello/despedida
+    [HttpGet("despedida")]
+    public string ByeWorld()
+    {
+        return "bye World";
+    }
+}
+
