0d43760eba990f6f1387d6d3ead05bce7746b6a8
Egolpe <enriquegolpe@gmail.com>
2022-11-07 11:12:06 +0100

    Proyecto ASP.NET con EFC

---
+++ ASPNET2/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.ChangeTracking;
+global using Microsoft.EntityFrameworkCore.Design;
+global using Microsoft.EntityFrameworkCore.Metadata;
+global using System.ComponentModel.DataAnnotations.Schema;
+
+global using System.ComponentModel.DataAnnotations;
+
+global using ASPNET2;
+global using ASPNET2.Db;
+global using EFC1.Models;
+global using EFC1.Reppositories;
