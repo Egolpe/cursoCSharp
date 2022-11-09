cf6c96e0d6ffc4ebc150a400e40451703cb3f483
Egolpe <enriquegolpe@gmail.com>
2022-11-09 11:28:17 +0100

    Category controller y address controller

---
+++ ASPNET2/Controllers/AddressController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ASPNET2.Controllers
+{
+    public class AddressController
+    {
+        public AddressController()
+        {
+        }
+    }
+}
+
---
+++ ASPNET2/Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ASPNET2.Controllers;
+
+public class CategoryController
+{
+    private ICategoryRepository CategoryRepo;
+
+
+    public CategoryController(ICategoryRepository categoryRepository)
+    {
+        CategoryRepo = categoryRepository;
+    }
+}
+
