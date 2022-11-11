6b60221b9418ea431af74de2b10e409d4ea24ad5
Egolpe <enriquegolpe@gmail.com>
2022-11-11 10:20:06 +0100

    angular model

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
 
-export interface Book {
-  id: number;
-  title: string;
-  author: string;
-  price: number;
-}
+
 
 
 
@@ -16,13 +11,7 @@ export interface Book {
 })
 export class AppComponent {
   title = 'angular';
-  books: Book[] = [
-    {id: 1, title: "Angular", author: "google", price: 100},
-    {id: 2, title: "CSharp", author: "Adecco", price: 54.33},
-    {id: 3, title: "React", author: "facebook", price: 90 },
-  ];
   
-  columnNames: string[] = ['Identificador', 'Título', 'Autor', 'Precio', 'Acciones']; 
 
 
 
--- Frontend/src/app/book-list/book-list.component.ts
+++ Frontend/src/app/book-list/book-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Book } from '../models/book.model';
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookListComponent implements OnInit {
 
+  books: Book[] = [
+    {id: 1, title: "Angular", author: "google", price: 100},
+    {id: 2, title: "CSharp", author: "Adecco", price: 54.33},
+    {id: 3, title: "React", author: "facebook", price: 90 },
+  ];
+  
+  columnNames: string[] = ['Identificador', 'Título', 'Autor', 'Precio', 'Acciones']; 
   constructor() { }
 
   ngOnInit(): void {
