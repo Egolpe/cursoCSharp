20470524ecc0f0836422a602ae393f38db2b9aa3
Egolpe <enriquegolpe@gmail.com>
2022-11-11 10:04:55 +0100

    routing

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookDetailComponent } from './book-detail/book-detail.component';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +22,12 @@ import { BookDetailComponent } from './book-detail/book-detail.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatTableModule,
-    MatIconModule
+    MatIconModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: 'book-list', pathMatch: 'full'},
+      {path: 'book-list', component: BookListComponent },
+      {path: 'book-detail', component: BookDetailComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
