2372664a1f84605e2ef4670a64adde0c2c0d4474
Egolpe <enriquegolpe@gmail.com>
2022-11-16 11:29:10 +0100

    angular author model and service

---
+++ Frontend/src/app/models/author.model.ts
@@ -0,0 +1,6 @@
+export interface Author {
+    id: number;
+    Fullname: string;
+    Email: string;
+    Salary: number;
+}
---
+++ Frontend/src/app/services/author.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Author } from '../models/author.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorService {
+  url = 'http://localhost:7219/api/Authors';
+  constructor(private http:HttpClient) { }
+
+  findAll() {
+    return this.http.get<Author[]>(this.url);
+  }
+
+  findById(id: number){
+    return this.http.get<Author>(`${this.url}/${id}`);
+  }
+
+  findByIdWithInclude(id: number){
+    return this.http.get<Author>(`${this.url}/include/${id}`)
+  }
+
+
+  create(Author: Author){
+    return this.http.post<Author>(this.url, Author);
+  }
+  //Borrar por id
+  deleteById(id: number) {
+    return this.http.delete(`${this.url}/${id}`);
+  }
+
+  update(Author: Author){
+    return this.http.put<Author>(this.url, Author);
+  }
+}
